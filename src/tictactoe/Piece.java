6929363f59c0a41cae99c86a5457c0ca209eab61
NatchSkikawe <natch.s@ku.th>
2018-08-17 21:12:48 +0700

    Fixed all issue

--- src/tictactoe/Piece.java
+++ src/tictactoe/Piece.java
@@ -13,7 +13,7 @@ import javafx.scene.text.TextAlignment;
  * 
  * Piece extends StackPane so it can be placed on another pane.
  * 
- * @author jim
+ * @author Mine now TriHard Clap
  *
  */
 public class Piece extends StackPane {
